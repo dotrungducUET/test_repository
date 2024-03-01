a5dc209e46f7270b1f4def2595a44f0705ab4164
dotrungducUET <160621205+dotrungducUET@users.noreply.github.com>
2024-03-01 18:44:59 +0800

    Add files via upload

---
+++ InitGame.h
@@ -0,0 +1,18 @@
+//#ifndef INITGAME_H_INCLUDED
+//#define INITGAME_H_INCLUDED
+//
+//#include <SDL.h>
+//#include "GameConstants.h"
+//
+//class Init
+//{
+//public:
+//    Init();
+//    bool InitGame();
+//    SDL_Window* window = NULL;
+//    SDL_Renderer* renderer = NULL;
+//};
+//
+//Init Init();
+//
+//#endif // INITGAME_H_INCLUDED
---
+++ Object.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_H_INCLUDED
+#define OBJECT_H_INCLUDED
+
+class object
+{
+    float x_pos;
+    float y_pos;
+    float width;
+    float height;
+
+};
+
+#endif // OBJECT_H_INCLUDED
